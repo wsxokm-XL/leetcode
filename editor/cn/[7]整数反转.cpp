4bc8ba0b4821327ea410a4d4dbc4e06c5b37afee
wsxokm-XL <84434556+wsxokm-XL@users.noreply.github.com>
2021-09-09 23:14:30 +0800

    leetcode7

---
+++ editor/cn/[7]整数反转.cpp
@@ -0,0 +1,77 @@
+//给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。 
+//
+// 如果反转后整数超过 32 位的有符号整数的范围 [−2³¹, 231 − 1] ，就返回 0。 
+//假设环境不允许存储 64 位整数（有符号或无符号）。
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：x = 123
+//输出：321
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：x = -123
+//输出：-321
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：x = 120
+//输出：21
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：x = 0
+//输出：0
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// -2³¹ <= x <= 2³¹ - 1 
+// 
+// Related Topics 数学 👍 3059 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    int reverse(int x) {
+        if(x==0) return x;
+        if(x==0-pow(2,31)) return 0;
+        int y,z;
+        if(x>0) {y=x;z=1;}
+        if(x<0) {y=0-x;z=-1;}
+
+        vector<int> nums;
+        int i=-1;
+
+        do{
+            i++;
+            nums.push_back(y%10);
+            y=y/10;
+        }while(y>0);
+
+        if(i==9&&nums[0]>2) return 0;
+
+        int res=0;
+        for(int j=i,k=0;j>=1;j--,k++){
+            res=res+nums[j]*pow(10,k);
+        }
+
+        if(i==9&&nums[0]==2&&z==1&&pow(2,31)-1-2000000000<res)   return 0;
+        else if(i==9&&nums[0]==2&&z==-1&&pow(2,31)-2000000000<res)   return 0;
+        else return z*(res+nums[0]*pow(10,i));
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
